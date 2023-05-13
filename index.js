8f67328435f2e6535d40c78f7f7f29be43bc7fed
Vladislav <voloshanowky@yandex.ru>
2023-05-13 18:10:05 +0300

    add telegram bot

---
+++ index.js
@@ -0,0 +1,39 @@
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
+require("dotenv").config();
+const dotenvExpand = require("dotenv-expand");
+
+const apiKey = process.env.API_KEY_TELEGRAM;
+const apiKeyYandex = process.env.API_KEY_YANDEX;
+
+const token = `${apiKey}`;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("voice", (msg) => {
+  const stream = bot.getFileStream(msg.voice.file_id);
+
+  let chunks = [];
+  stream.on("data", (chunk) => chunks.push(chunk));
+  stream.on("end", () => {
+    const axiosConfig = {
+      method: "POST",
+      url: "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize",
+      headers: {
+        Authorization: "Api-Key " + apiKeyYandex,
+      },
+      data: Buffer.concat(chunks),
+    };
+    try {
+      axios(axiosConfig).then((response) => {
+          const command = response.data.result;
+          bot.sendMessage(msg.chat.id, command);
+      });
+        setTimeout(() => {
+          bot.sendMessage(msg.chat.id, "🔥🔥🔥");
+        },2000)
+    } catch (error) {
+      console.log("ошибка распознования речи", error);
+    }
+  });
+});
