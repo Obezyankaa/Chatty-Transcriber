b3a1803c0b6d4f306529a7bba5007f8b7aeecad2
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2024-10-25 22:13:49 +0400

    add i18next

--- bot.ts
+++ bot.ts
@@ -3,11 +3,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const token = process.env.TOKEN_TELEGRAM;
+
+
+const token = process.env.BOT_TOKEN;
 
 const bot = new Bot(String(token));
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+bot.on("message", async (ctx) => {
+    ctx.reply("Bot can't answer. You need use commands or Telegram mini app!")
+});
+
+bot.on("edited_message", async (ctx) => {
+  // Получите новый, отредактированный текст сообщения.
+    const editedText = ctx.editedMessage.text;
+    console.log(editedText, '👈👈 editedText');
+    ctx.reply("sms refactoring")
+});
+
 
 bot.start();
---
+++ locales/en.ftl
@@ -0,0 +1,2 @@
+start = Welcome!
+message = Bot can't answer. You need use commands or Telegram mini app!
